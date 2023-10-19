826824644dcb802b3e2d204248e48e6c81e59072
SeyidovSirac <60558048+SiracSeyidov@users.noreply.github.com>
2023-10-19 21:24:36 +0300

    Initial Commit

---
+++ TradingView/TradingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingViewApp.swift
+//  TradingView
+//
+//  Created by Sirac Seyidov on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TradingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
